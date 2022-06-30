0bc89476ac9812dff01918fb2f320026b95b520d
Taha <tahahmmcgl@gmail.com>
2022-06-30 15:46:39 +0300

    Echo downloaded

---
+++ cmd/gorest/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/tahahmmcgl/gorest/pkg/api"
+)
+
+func main() {
+	fmt.Println("taha")
+
+	api.Api()
+	a := ""
+	fmt.Scanln(&a)
+	fmt.Println("end")
+}
---
+++ pkg/api/api.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func Api() {
+	fmt.Println("api")
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+
+		return c.String(http.StatusOK, "Hello World")
+	})
+	e.Start(":8080")
+}
